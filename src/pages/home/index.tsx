1acba9d6a62b192f364aa2a905d1d05a644bafc7
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-04-13 13:12:17 -0300

    Criação do componente social

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,6 @@
+import { FaGithub, FaInstagram, FaLinkedin, FaPortrait } from "react-icons/fa";
+import { Social } from "../../components/Social";
+
 export function Home() {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
@@ -9,11 +12,27 @@ export function Home() {
       <main className="flex flex-col w-11/12 max-w-xl text-center">
         <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
           <a href="#">
-            <p className="text-base md:text-lg">Canal no youtube</p>
+            <p className="text-base md:text-lg">Portfólio</p>
           </a>
         </section>
 
-        <footer className="flex justify-center gap-3 my-4"></footer>
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url={`https://gilcarvalhal.github.io/gil/`}>
+            <FaPortrait size={35} color="#fff" />
+          </Social>
+
+          <Social url={`https://www.instagram.com/gilcarvalhal1/`}>
+            <FaInstagram size={35} color="#fff" />
+          </Social>
+
+          <Social url={`https://www.linkedin.com/in/gilberto-carvalhal/`}>
+            <FaLinkedin size={35} color="#fff" />
+          </Social>
+
+          <Social url={`https://github.com/gilcarvalhal`}>
+            <FaGithub size={35} color="#fff" />
+          </Social>
+        </footer>
       </main>
     </div>
   );
