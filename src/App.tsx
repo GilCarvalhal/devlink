fe23c8b03f36b247a5252dbe1944bd204f238fbf
gilberto.matos <gilberto.matos@salvador.ba.gov.br>
2025-04-14 11:29:16 -0300

    Proteção da rota de admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import { Home } from "./pages/home";
-import { Login } from "./pages/login";
 import { Admin } from "./pages/admin";
+import { Login } from "./pages/login";
 import { Networks } from "./pages/networks";
+
+import { Private } from "./routes/Private";
 
 const router = createBrowserRouter([
   {
@@ -15,11 +18,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <Admin />,
+    element: (
+      <Private>
+        <Admin />
+      </Private>
+    ),
   },
   {
     path: "/admin/social",
-    element: <Networks />,
+    element: (
+      <Private>
+        <Networks />
+      </Private>
+    ),
   },
 ]);
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,35 +1,37 @@
-import { Link, replace, useNavigate } from "react-router-dom";
-import { Input } from "../../components/Input";
 import { FormEvent, useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../../components/Input";
+
 import { auth } from "../../services/firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
     if (email === "" || password === "") {
-      alert(`Preencha todos os campos!`);
+      alert("Preencha todos os campos!");
       return;
     }
+
     signInWithEmailAndPassword(auth, email, password)
       .then(() => {
-        console.log(`Logado com sucesso!`);
+        console.log("LOGADO COM SUCESSO!");
         navigate("/admin", { replace: true });
       })
       .catch((error) => {
-        console.log(`Erro ao efetuar o login: ${error}`);
+        console.log("ERRO AO FAZER O LOGIN:");
+        console.log(error);
       });
   }
 
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
-      <Link to={`/`}>
+      <Link to="/">
         <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
           Dev
           <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
@@ -43,14 +45,14 @@ export function Login() {
         className="w-full max-w-xl flex flex-col px-2"
       >
         <Input
-          placeholder="Digite o seu e-mail..."
+          placeholder="Digite o seu email..."
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
-          placeholder="Digite a sua senha..."
+          placeholder="*********"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -58,7 +60,7 @@ export function Login() {
 
         <button
           type="submit"
-          className="h-9 bg-blue-600 text-white rounded border-0 text-lg font-medium cursor-pointer"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
         >
           Acessar
         </button>
