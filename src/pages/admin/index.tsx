78fdea244b7e727c6d2536cf71abad7a001028b6
gilberto.matos <gilberto.matos@salvador.ba.gov.br>
2025-04-15 10:56:19 -0300

    Finalizando a interface do formulário

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { FiTrash } from "react-icons/fi";
 
 export function Admin() {
   const [nameInput, setNameInput] = useState("");
@@ -51,24 +52,47 @@ export function Admin() {
           </div>
         </section>
 
-        <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-          <label className="text-white font-medium mt-8 mb-3">
-            Veja como está ficando:
-          </label>
-          <article
-            className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
-            style={{
-              marginBottom: 8,
-              marginTop: 8,
-              backgroundColor: backgroundColorInput,
-            }}
-          >
-            <p className="font-medium" style={{ color: textColorInput }}>
-              Instagram
-            </p>
-          </article>
-        </div>
+        {nameInput !== "" && (
+          <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+            <label className="text-white font-medium mt-8 mb-3">
+              Veja como está ficando:
+            </label>
+            <article
+              className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
+              style={{
+                marginBottom: 8,
+                marginTop: 8,
+                backgroundColor: backgroundColorInput,
+              }}
+            >
+              <p className="font-medium" style={{ color: textColorInput }}>
+                {nameInput}
+              </p>
+            </article>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="bg-blue-600 mb-7 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center"
+        >
+          Cadastrar
+        </button>
       </form>
+
+      <h2 className="font-bold text-white mb-4 text-2xl">Meus links</h2>
+
+      <article
+        className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 select-none"
+        style={{ backgroundColor: "#2563eb", color: "#fff" }}
+      >
+        <p>Link do instagram</p>
+        <div>
+          <button className="border border-dashed p-1 rounded bg-neutral-900">
+            <FiTrash size={18} color="#fff" />
+          </button>
+        </div>
+      </article>
     </div>
   );
 }
